f5eb23abe37431bf0c4a441c8dd46be2f8784370
DCsunset <DCsunset@163.com>
2019-04-12 17:16:11 +0800

    Update Toolbar component

--- src/build-entry.js
+++ src/build-entry.js
@@ -1,14 +1,17 @@
 import MarkdownEditor from './components/MarkdownEditor.vue';
+import Toolbar from './components/Toolbar.vue';
 import marked from './util/marked.js';
 
 // Named export
 export {
 	MarkdownEditor as Editor,
+	Toolbar as Toolbar,
 	marked as Renderer
 };
 
 // Default export
 export default {
 	Editor: MarkdownEditor,
+	Toolbar,
 	Renderer: marked
 };
