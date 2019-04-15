c5f88ddda9fd46fc2d6bb30a39375fa89980ed2f
DCsunset <DCsunset@163.com>
2019-04-15 20:44:06 +0800

    Add outline and editor-only mode

--- src/components/MarkdownEditor.vue
+++ src/components/MarkdownEditor.vue
@@ -1,13 +1,12 @@
 <template>
 	<v-container fluid>
-    <v-layout row fill-height wrap>
-      <v-flex xs12 md6 fill-height class="pa-3">
-        <v-layout column fill-height>
-          <v-card>
-            <toolbar
-              height="42px"
-              @emoji="insertEmoji"
-              />
+    <v-layout row wrap>
+      <v-flex xs12 :md6="preview" class="pa-3">
+        <v-layout column>
+          <v-card v-if="!outline">
+            <v-toolbar flat>
+              <toolbar @emoji="insertEmoji" />
+            </v-toolbar>
             <v-textarea
               solo
               flat
@@ -18,39 +17,68 @@
               @input="val => $emit('input', val)"
               />
           </v-card>
+
+          <template v-else>
+            <div class="pa-2 outline">
+              <toolbar @emoji="insertEmoji" />
+            </div>
+            <div class="outline" style="border-top: none">
+              <v-textarea
+                solo
+                flat
+                hide-details
+                auto-grow
+                ref="textarea"
+                :value="value"
+                @input="val => $emit('input', val)"
+                />
+            </div>
+          </template>
         </v-layout>
       </v-flex>
-      <v-flex xs12 md6 fill-height class="pa-3">
-        <v-card>
+
+      <v-flex v-if="preview" xs12 :md6="preview" class="pa-3">
+        <v-card v-if="!outline">
           <v-card-text v-if="compiled && mode === 'Rendered'" class="subheading markdown-text" v-html="compiled" />
           <v-card-text v-else-if="compiled && mode === 'Source'" class="subheading">
             {{ compiled }}
           </v-card-text>
         </v-card>
+
+        <div v-else class="pa-3 outline">
+          <div v-if="compiled && mode === 'Rendered'" class="subheading markdown-text" v-html="compiled" />
+          <div v-else-if="compiled && mode === 'Source'" class="subheading">
+            {{ compiled }}
+          </div>
+        </div>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <style scoped>
-/* Remove boder-radius */
+/* Remove border-radius */
 .v-textarea >>> .v-input__control > .v-input__slot
 {
   border-top-left-radius: 0;
   border-top-right-radius: 0;
 }
+
+.outline
+{
+  border: 1.5px solid #c8ccd0;
+}
 </style>
 
 <script>
 import marked from '../util/marked.js';
-import { VContainer, VLayout, VFlex, VCard, VCardText, VTextarea } from 'vuetify/lib';
+import { VContainer, VToolbar, VLayout, VFlex, VCard, VCardText, VTextarea } from 'vuetify/lib';
 // Styles
-import 'vuetify/src/stylus/app.styl';
-import '../style.css';
 import Toolbar from './Toolbar.vue';
 
 export default {
   components: {
+    VToolbar,
     VContainer,
     VLayout,
     VFlex,
@@ -67,6 +95,14 @@ export default {
     mode: {
       type: String,
       default: 'Rendered'
+    },
+    outline: {
+      type: Boolean,
+      default: false
+    },
+    preview: {
+      type: Boolean,
+      default: true
     }
   },
   computed: {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify)
+
+import 'vuetify/src/stylus/app.styl';
+import './style.css';
 
 new Vue({
   render: h => h(App),
