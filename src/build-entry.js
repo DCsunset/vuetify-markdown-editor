991ae1aee364b6c70b712c9ccca945aad8cce08a
DCsunset <dcsunset@protonmail.com>
2020-02-05 02:19:01 -0800

    fix: fix build-entry

--- src/build-entry.js
+++ src/build-entry.js
@@ -1,17 +1,14 @@
 import MarkdownEditor from './components/MarkdownEditor.vue';
-import Toolbar from './components/Toolbar.vue';
 import render from './util/render.js';
 
 // Named export
 export {
 	MarkdownEditor as Editor,
-	Toolbar as Toolbar,
 	render as Renderer
 };
 
 // Default export
 export default {
 	Editor: MarkdownEditor,
-	Toolbar,
 	Renderer: render
 };
