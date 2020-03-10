8248ac551134ac18ba139eaabd74dbc6b265abab
DCsunset <dcsunset@protonmail.com>
2020-03-10 02:05:03 -0700

    feat: add TypeScript support

---
+++ types/index.d.ts
@@ -0,0 +1,15 @@
+export declare function Renderer(
+	text: string,
+	customOptions?: {
+		katex?: boolean,
+		mermaid?: boolean,
+		inline?: boolean
+	},
+	customConfig?: {
+		katex?: any,
+		mermaid?: any
+	},
+	renderCache?: any
+): string;
+
+export declare class Editor extends Vue { };
