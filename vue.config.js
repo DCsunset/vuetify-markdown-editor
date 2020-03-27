4bd04f68c869f57196e024d2f851bb05afb0e664
DCsunset <dcsunset@protonmail.com>
2020-03-26 23:47:02 -0700

    perf: reduce library size

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,9 @@ const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 
 module.exports = {
 	configureWebpack: {
-		plugins: [
+		plugins: process.env.NODE_ENV !== 'production' ? [
 			new VuetifyLoaderPlugin()
-		],
+		] : [],
 		output: {
 			libraryExport: 'default'
 		}
